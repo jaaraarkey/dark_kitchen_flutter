fdf03114df16ffce19a5cd880ac9e3e727de74ee
jaaraarkey <olegudyachenko@gmail.com>
2024-07-22 17:44:16 +0200

    improved UI

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -10,28 +10,44 @@ class MainDrawer extends StatelessWidget {
         child: Column(
           children: [
             DrawerHeader(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                    Theme.of(context).colorScheme.primary,
-                    Theme.of(context).colorScheme.primary.withOpacity(0.9)
-                  ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-                ),
-                child: Row(
-                  children: [
-                    Icon(Icons.restaurant,
-                        size: 36,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Theme.of(context).colorScheme.primary,
+                  Theme.of(context).colorScheme.primary.withOpacity(0.9)
+                ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+              ),
+              child: Row(
+                children: [
+                  // Icon(Icons.restaurant,
+                  //     size: 36, color: Theme.of(context).colorScheme.onPrimary),
+                  // const SizedBox(width: 16),
+                  Text(
+                    'Menu',
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
                         color: Theme.of(context).colorScheme.onPrimary),
-                    const SizedBox(width: 16),
-                    Text(
-                      'Cooking Up!',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headlineSmall!
-                          .copyWith(
-                              color: Theme.of(context).colorScheme.onPrimary),
-                    )
-                  ],
-                )),
+                  )
+                ],
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.restaurant, size: 24),
+              title: Text('Meals',
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface)),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings, size: 24),
+              title: Text('Settings',
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface)),
+              onTap: () {
+                // todo implement settings screen, routing
+                ;
+              },
+            ),
           ],
         ));
   }
