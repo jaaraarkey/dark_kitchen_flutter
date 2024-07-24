0ee6d65a2d4bb63a2e2952956fa494ccf14ad1d5
jaaraarkey <olegudyachenko@gmail.com>
2024-07-24 17:16:05 +0200

    state_provider

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -1,9 +1,14 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:black_kitchen/models/meal.dart';
 
+/// A notifier class that manages the list of favorite meals.
 class FavoritesNotifier extends StateNotifier<List<Meal>> {
   FavoritesNotifier() : super([]);
 
+  /// Toggles the favorite status of a meal.
+  ///
+  /// If the meal is already a favorite, it will be removed from the list of favorites.
+  /// If the meal is not a favorite, it will be added to the list of favorites.
   void toggleFavoriteMealStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
 
@@ -15,8 +20,8 @@ class FavoritesNotifier extends StateNotifier<List<Meal>> {
   }
 }
 
+/// A provider that creates and manages an instance of [FavoritesNotifier].
 final favoritesProvider =
     StateNotifierProvider<FavoritesNotifier, List<Meal>>((ref) {
   return FavoritesNotifier();
 });
-// Compare this snippet from lib/widgets/meal_item.dart:
